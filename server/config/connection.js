d744682e9b8680d8fa785451eefeb008cc14b3a2
Sona <sonali23.1293@gmail.com>
2021-09-13 13:13:58 -0400

    unhandled promise mongo resolved

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/next-read', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    // useFindAndModify: false, // resolved the unhandled promise issue and the useFindAndModify not supported warning
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
